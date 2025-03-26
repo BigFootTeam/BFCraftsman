7b4e015ba25acda8e9195750ae899f569dd25884
enderneko <enderneko-dev@outlook.com>
2025-03-26 19:15:27 +0800

    Update zhCN

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,2 +1,51 @@
 if not LOCALE_zhCN then return end
 local L = select( 2, ...).L
+
+L["BFCraftsman"] = "大脚工匠"
+L["Browse"] = "浏览"
+L["Publish"] = "发布"
+
+-- publish
+L["Enable Publishing"] = "启用发布"
+L["Publishing is disabled"] = "发布已禁用"
+L["Tagline"] = "简介"
+L["Syncs automatically every few minutes instead of in real time"] = "发布不是实时的，而是每几分钟自动同步一次"
+L["Characters and Professions"] = "角色和专业"
+L["Add Current Character"] = "添加当前角色"
+L["Delete Character"] = "删除角色"
+L["Alt-Click to delete"] = "Alt点击删除"
+L["never"] = "从未"
+L["Scan Recipes"] = "扫描配方"
+L["Click to scan recipes"] = "点击扫描配方"
+L["Scan only available for current character"] = "只能扫描当前角色的配方"
+L["Learned recipes: %s"] = "已学会的配方：%s"
+L["Last scanned: %s"] = "最后扫描：%s"
+L["Scanning..."] = "扫描中..."
+L["Saving..."] = "保存中..."
+
+-- chat config
+L["Disabled by BFCraftsman"] = "已被 BFCraftsman 禁用"
+
+-- list
+L["Show Stale"] = "显示已过期"
+L["Show Blacklisted"] = "显示已屏蔽"
+L["Stale"] = "已过期"
+L["Blacklisted"] = "已屏蔽"
+L["Last updated: %s"] = "最后更新：%s"
+L["Find Craftsmen"] = "查找工匠"
+L["Craftsmen List"] = "工匠列表"
+
+-- detail
+L["Name"] = "名字"
+L["Blacklist"] = "屏蔽"
+L["The blacklist button in the list has the same functionality"] = "列表中的屏蔽按钮也具有相同的功能"
+L["add to blacklist"] = "添加至屏蔽列表"
+L["also remove from list"] = "同时从列表中移除"
+L["Send Whisper"] = "发送密语"
+L["for reporting inappropriate user content"] = "用于报告违规发布"
+L["Details"] = "详情"
+L["Can Craft?"] = "会制作吗？"
+L["Can Craft"] = "会制作"
+L["Cannot Craft"] = "不会制作"
+L["Checking..."] = "检查中..."
+L["Timeout"] = "超时"
